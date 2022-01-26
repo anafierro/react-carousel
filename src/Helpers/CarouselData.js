dca57e1e7568a106fdedc5d24331755800c3ff44
anafierro <anafierro190498@gmail.com>
2022-01-26 11:53:12 -0600

    first commit

---
+++ src/Helpers/CarouselData.js
@@ -0,0 +1,21 @@
+import Yoda1 from '../images/1.jpeg';
+import Yoda2 from '../images/2.webp';
+import Yoda3 from '../images/3.jpeg';
+
+export const images = [
+    {
+        title: "Yoda",
+        subtitle: "This is Yoda 1",
+        img: Yoda1
+    },
+    {
+        title: "Yoda eating",
+        subtitle: "This is Yoda 2",
+        img: Yoda2
+    },
+    {
+        title: "Angry Yoda",
+        subtitle: "This is Yoda 3",
+        img: Yoda3
+    }
+]
